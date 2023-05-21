f3069213ec4917f32b92f65e2ef9b02595866f25
Sabbir386 <sabbir386.sa@gmail.com>
2023-05-21 16:13:29 +0600

    firebase config hide

---
+++ README.md
@@ -0,0 +1,10 @@
+## Features
+
+
+ Live Site : https://the-master-chef.web.app/
+
+- **Technology:** React Js,express server. 
+- **Home Page:** We can see navbar and header section and Chef section and Reasonable footer Section.
+- **ChefRecipes page:** Chef Recipes page are private if a user want to see recipes he/she first need login .
+- **Login and registration Page:** Login and Registration Page managed by Firebase Authentication System.
+- **Blog section:** Give answered four question.
--- src/Components/firebase/firebase.config.js
+++ src/Components/firebase/firebase.config.js
@@ -5,12 +5,19 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDqN9MdI1KAZtKB7pZuAfzBs5Y_xAa5_Bg",
-    authDomain: "brain-boosters-project.firebaseapp.com",
-    projectId: "brain-boosters-project",
-    storageBucket: "brain-boosters-project.appspot.com",
-    messagingSenderId: "802388134448",
-    appId: "1:802388134448:web:8e0fac2142dd9e88e57bf5"
+    // apiKey: "AIzaSyDqN9MdI1KAZtKB7pZuAfzBs5Y_xAa5_Bg",
+    // authDomain: "brain-boosters-project.firebaseapp.com",
+    // projectId: "brain-boosters-project",
+    // storageBucket: "brain-boosters-project.appspot.com",
+    // messagingSenderId: "802388134448",
+    // appId: "1:802388134448:web:8e0fac2142dd9e88e57bf5"
+
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
